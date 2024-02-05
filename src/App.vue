42bc97634a1a7d84b0c43635e998776902ccb6c9
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-02-05 11:50:21 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <div class="App container">
+    <Header></Header>
+  </div>
+</template>
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <h1>Minha calculadora</h1>
+    <span>Você fez um total de <strong>5</strong> cálculos até o momento</span>
+  </header>
+</template>
